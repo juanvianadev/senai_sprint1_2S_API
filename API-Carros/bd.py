ac7cf01ab4da9e2f7a45df21699e562cb5cc3afc
Juan Viana Lorenzo <47398092814@info.local>
2024-08-22 18:32:44 -0300

    API de carros criada

---
+++ API-Carros/bd.py
@@ -0,0 +1,36 @@
+Carros = [
+    {
+        "id" : 1,
+        "marca" : "Honda",
+        "modelo" : "Civic",
+        "ano" : 2000
+    },
+
+    {
+        "id" : 2,
+        "marca" : "Fiat",
+        "modelo" : "Palio",
+        "ano" : 1997
+    },
+
+    {
+        "id" : 3,
+        "marca" : "BMW",
+        "modelo" : "M5",
+        "ano" : 1990
+    },
+
+    {
+        "id" : 4,
+        "marca" : "Volkswagen",
+        "modelo" : "Fusca",
+        "ano" : 1970
+    },
+
+    {
+        "id" : 5,
+        "marca" : "Mitsubishi",
+        "modelo" : "Lancer",
+        "ano" : 1990
+    }
+]
