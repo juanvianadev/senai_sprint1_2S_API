6cb90b2d0952dc06f5fa76d556131cd437bba553
Juan Viana Lorenzo <47398092814@info.local>
2024-08-22 21:17:02 -0300

    API CARROS FINALIZADA

--- API-Carros/main.py
+++ API-Carros/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, request
 from bd import Carros
 
 app = Flask("carros")
@@ -6,3 +6,36 @@ app = Flask("carros")
 @app.route("/carros", methods=["GET"])
 def getCarros():
     return Carros
+
+@app.route("/carros/<int:id>", methods=["GET"])
+def getCarrosID(id):
+    for carro in Carros:
+        if carro.get("id") == id:
+            return jsonify(carro)
+        
+@app.route("/carros", methods=["POST"])
+def criarCarros():
+    carro = request.json
+    Carros.append(carro)
+    return make_response(
+        jsonify(mensagem = "Carro cadastrado com sucesso!" , carro = carro)
+    )
+
+@app.route("/carros/<int:id>", methods=["PUT"])
+def editarCarrosID(id):
+    carroAlterado = request.get_json()
+    for indice, carro in enumerate(Carros):
+        if carro.get('id') == id:
+            Carros[indice].update(carroAlterado)
+            return jsonify(Carros[indice])
+
+@app.route("/carros/<int:id>", methods=["DELETE"])
+def excluirCarrosID(id):
+    for indice, carro in enumerate(Carros):
+        if carro.get('id') == id:
+            del Carros[indice]
+            return make_response(
+                jsonify(mensagem = "Carro excluido com sucesso!")
+            )
+
+app.run(port=5000, host="localhost")
---
+++ ProjetoIntegrador/bd.py
@@ -0,0 +1,48 @@
+Dados = [
+    {
+        "id" : 1,
+        "co2": 30.9,
+        "temperatura": 24,
+        "pressao": 100,
+        "umidade": 32.4,
+        "altitude": 10
+    },
+
+    {
+        "id" : 2,
+        "co2": 34.9,
+        "temperatura": 26,
+        "pressao": 99,
+        "umidade": 35.2,
+        "altitude": 10
+    },
+
+    {
+        "id" : 3,
+        "co2": 26.8,
+        "temperatura": 25,
+        "pressao": 95,
+        "umidade": 33.2,
+        "altitude": 10
+    },
+
+    {
+        "id" : 4,
+        "co2": 33.9,
+        "temperatura": 27,
+        "pressao": 97,
+        "umidade": 31.9,
+        "altitude": 10
+    },
+
+    {
+        "id" : 5,
+        "co2": 25.6,
+        "temperatura": 22,
+        "pressao": 98,
+        "umidade": 35.4,
+        "altitude": 10
+    },
+
+    
+]
